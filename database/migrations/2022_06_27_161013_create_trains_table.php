0a06cc476e90b90b1fbec65648c11545ca9ca879
RiccardoBeca <riccardobeca@gmail.com>
2022-06-27 17:11:51 +0100

    migration succesful

--- database/migrations/2022_06_27_161013_create_trains_table.php
+++ database/migrations/2022_06_27_161013_create_trains_table.php
@@ -15,6 +15,15 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->time('orario_di_partenza', 4);
+            $table->time('orario_di_arrivo', 4);
+            $table->string('codice_treno', 50);
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
